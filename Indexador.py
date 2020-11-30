aa12a9515fe3be792f18067151ad9cc6134b9d56
Diego Farias Castro <diegofariascastro@MacBook-Air-de-Diego.local>
2020-11-30 08:17:54 -0600

    Finalizado el indexador

--- Indexador.py
+++ Indexador.py
@@ -7,6 +7,7 @@ from Transacciones  import ModuloTransacciones
 from LSI            import IndiceLSI
 from datetime       import datetime
 from os             import system
+from Consultas      import Consultas
 
 class Indexador:
 
@@ -30,7 +31,34 @@ class Indexador:
 
             # Cargar el documento y enviarlo a lematizar
             archivo = open( documento , "r")
-            print(self.__lematizador.lematizarTexto( archivo ))
+            campos = archivo.read().split("\n<!campo>\n")
+
+            # Insertar documento en la base de datos
+            id = self.__moduloTransacciones.insertarDocumento( 
+                campos[0], campos[1], campos[2][:508] + "...", campos[3], campos[4], 
+                campos[5], campos[6], campos[7], campos[8] 
+            ) # End insertar documento
+
+            # Notificar registro en la base de datos
+            print(("[!] Documento %s registrado en la base de datos con id: %s") % ( documento, id ))
+
+            # Lematizar textos
+            textoLematizado = self.__lematizador.lematizarTexto( campos[9] )
+            
+            # Cargar terminos de indexacion a la BD y registrar sus apariciones
+            for posicion, palabra in enumerate(textoLematizado.split(" ")):
+
+                # Revisar si la palabra tiene alguna relacion para lematizar
+                # si no, pasarla directamente como termino
+
+                termino = palabra
+                self.__moduloTransacciones.insertarTermino(termino)
+                self.__moduloTransacciones.registrarAparicion(id, termino, posicion)
+                
+            print(("[!] Documento %s lematizado e indexado correctamente") % (id))
+
+        # Fabricar el LSI
+        Consultas.establecerLSI()
 
         print("[!] Indexacion completada")
         input()
